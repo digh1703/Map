a270bef7fd5ed01ddc65c6653fef839948c2785a
digh1703 <digh1703@naver.com>
2020-05-17 06:31:57 +0900

    Initial commit

---
+++ app/src/main/java/com/sentiMap/MapsNaverActivity.java
@@ -0,0 +1,206 @@
+package com.sentiMap;
+
+import android.app.Activity;
+import android.graphics.PointF;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.naver.maps.geometry.LatLng;
+import com.naver.maps.map.MapFragment;
+import com.naver.maps.map.MapView;
+import com.naver.maps.map.NaverMap;
+import com.naver.maps.map.OnMapReadyCallback;
+import com.naver.maps.map.UiSettings;
+import com.naver.maps.map.overlay.InfoWindow;
+import com.naver.maps.map.overlay.Marker;
+import com.naver.maps.map.overlay.Overlay;
+import com.naver.maps.map.overlay.OverlayImage;
+import com.naver.maps.map.util.FusedLocationSource;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class MapsNaverActivity extends AppCompatActivity implements NaverMap.OnMapClickListener, Overlay.OnClickListener, OnMapReadyCallback, NaverMap.OnCameraChangeListener, NaverMap.OnCameraIdleListener {
+
+    private static final int ACCESS_LOCATION_PERMISSION_REQUEST_CODE=100;
+    private FusedLocationSource locationSource;
+    private NaverMap naverMap;
+    private InfoWindow infoWindow;
+    private List<Marker> markerList=new ArrayList<>();
+    private boolean isCameraAnimated=false;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps_naver);
+
+        MapFragment mapFragment = (MapFragment)getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);//지도데이터를 비동기적으로 불러온다.
+    }
+
+    @Override
+    public void onMapReady(@NonNull NaverMap naverMap) {
+          this.naverMap = naverMap;
+          locationSource=new FusedLocationSource(this,ACCESS_LOCATION_PERMISSION_REQUEST_CODE);
+          naverMap.setLocationSource(locationSource);
+          UiSettings uiSettings=naverMap.getUiSettings();
+          uiSettings.setLocationButtonEnabled(true);
+
+          naverMap.addOnCameraChangeListener(this);
+          naverMap.addOnCameraIdleListener(this);
+
+          infoWindow=new InfoWindow();
+
+          infoWindow.setAdapter(new InfoWindow.DefaultViewAdapter(this) {
+              @NonNull
+              @Override
+              protected View getContentView(@NonNull InfoWindow infoWindow) {
+                  Marker marker=infoWindow.getMarker();
+                  Store store=(Store)marker.getTag();
+                  View view=View.inflate(MapsNaverActivity.this,R.layout.view_info_windwo,null);
+                  ((TextView) view.findViewById(R.id.name)).setText(store.name);
+                  if("plenty".equalsIgnoreCase(store.remain_stat)){
+                      ((TextView) view.findViewById(R.id.stock)).setText("100개 이상");
+                  }
+                  else if("some".equalsIgnoreCase(store.remain_stat)){
+                      ((TextView) view.findViewById(R.id.stock)).setText("30 이상 100개 미만");
+                  }
+                  else if("few".equalsIgnoreCase(store.remain_stat)){
+                      ((TextView) view.findViewById(R.id.stock)).setText("2개 이상 30개 미만");
+                  }
+                  else if("empty".equalsIgnoreCase(store.remain_stat)){
+                      ((TextView) view.findViewById(R.id.stock)).setText("1개 이하");
+                  }
+                  else{
+                      ((TextView) view.findViewById(R.id.stock)).setText("판매중지");
+                  }
+                      ((TextView) view.findViewById(R.id.time)).setText("입고"+store.stock_at);
+                      return view;
+              }
+          });
+
+          LatLng mapCenter=naverMap.getCameraPosition().target;
+          fetchStoreSale(mapCenter.latitude,mapCenter.longitude,5000);
+    }
+
+    @Override
+    public boolean onClick(@NonNull Overlay overlay) {
+        if (overlay instanceof Marker) {
+            Marker marker = (Marker) overlay;
+            if (marker.getInfoWindow() != null) {
+                infoWindow.close();
+            } else {
+                infoWindow.open(marker);
+            }
+            return true;
+        }
+        return false;
+    }
+
+
+    public void onRequestPermissionReuslt(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults){
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch(requestCode){
+            case ACCESS_LOCATION_PERMISSION_REQUEST_CODE:
+                locationSource.onRequestPermissionsResult(requestCode, permissions, grantResults);
+                return;
+
+        }
+
+    }
+    @Override
+    public void onCameraChange(int reason, boolean animated){
+        isCameraAnimated=animated;
+    }
+
+    @Override
+    public void onCameraIdle(){
+        if(isCameraAnimated){
+            LatLng mapCenter=naverMap.getCameraPosition().target;
+            fetchStoreSale(mapCenter.latitude,mapCenter.longitude,5000);
+        }
+
+    }
+
+
+    private void fetchStoreSale(double lat, double lng, int m){
+        Retrofit retrofit=new Retrofit.Builder().baseUrl("https://8oi9s0nnth.apigw.ntruss.com").addConverterFactory(GsonConverterFactory.create()).build();
+        MaskApi maskApi = retrofit.create(MaskApi.class);
+        maskApi.getStoresByGeo(lat, lng, m).enqueue(new Callback<StoreSaleResult>() {
+            @Override
+            public void onResponse(Call<StoreSaleResult> call, Response<StoreSaleResult> response) {
+                if(response.code()==200){
+                    StoreSaleResult result =response.body();
+                    updateMapMarkers(result);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<StoreSaleResult> call, Throwable t) {
+
+            }
+        });
+
+    }
+
+    private void updateMapMarkers(StoreSaleResult result){
+        resetMarkserList();
+        if(result.stores !=null&& result.stores.size()>0){
+            for(Store store: result.stores){
+                Marker marker = new Marker();
+                marker.setTag(store);
+                marker.setPosition(new LatLng(store.lat, store.lng));
+                if("plenty".equalsIgnoreCase(store.remain_stat)){
+                    marker.setIcon(OverlayImage.fromResource(R.drawable.marker_green));
+                }
+                else if("some".equalsIgnoreCase(store.remain_stat)){
+                    marker.setIcon(OverlayImage.fromResource(R.drawable.marker_blue));
+                }
+                else if("few".equalsIgnoreCase(store.remain_stat)){
+                    marker.setIcon(OverlayImage.fromResource(R.drawable.marker_red));
+                }
+                else{
+                    marker.setIcon(OverlayImage.fromResource(R.drawable.marker_black));
+                }
+                marker.setAnchor(new PointF(0.5f,1.0f));
+                marker.setMap(naverMap);
+                marker.setOnClickListener(this);
+                markerList.add(marker);
+
+            }
+
+        }
+
+    }
+
+    private void resetMarkserList(){
+        if(markerList!=null&&markerList.size()>0){
+            for(Marker marker : markerList){
+                marker.setMap(null);
+            }
+            markerList.clear();
+        }
+    }
+
+    @Override
+    public void onMapClick(@NonNull PointF pointF, @NonNull LatLng latLng) {
+        if(infoWindow.getMarker()!=null){
+            infoWindow.close();
+        }
+    }
+
+
+}
---
+++ app/src/main/java/com/sentiMap/StoreSaleResult.java
@@ -0,0 +1,8 @@
+package com.sentiMap;
+
+import java.util.List;
+
+public class StoreSaleResult {
+    public int count;
+    public List<Store> stores;
+}
